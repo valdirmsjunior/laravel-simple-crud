9d9d05907ff0ba5a6c1e64de686ae623548cfa87
Valdir Júnior <valdirmsjunior@gmail.com>
2025-09-18 14:25:21 -0300

    add factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes;
+    use HasFactory;
 
     protected $fillable = ['name', 'status'];
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         "name",
